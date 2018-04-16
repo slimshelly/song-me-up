70992764feec5ce52f0a4582d3f1eb0a5b5670df
tvanderm <thomas_vandermillen@brown.edu>
2018-04-16 18:24:20 -0400

    TODO comment for Song class

--- src/main/java/edu/brown/cs/jmst/music/Song.java
+++ src/main/java/edu/brown/cs/jmst/music/Song.java
@@ -1,8 +1,5 @@
 package edu.brown.cs.jmst.music;
 
 public class Song {
-  private String id;
-  //private Artist artist;
-  //private Album album;
-  
+  //TODO: add fields for every field accessible from Spotify API
 }
