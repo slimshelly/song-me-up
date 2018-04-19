dfb727363ae11a5a38cd036f7740001448b46af6
tvanderm <thomas_vandermillen@brown.edu>
2018-04-19 19:14:43 -0400

    class Song extends Entity

--- src/main/java/edu/brown/cs/jmst/music/Song.java
+++ src/main/java/edu/brown/cs/jmst/music/Song.java
@@ -1,6 +1,8 @@
 package edu.brown.cs.jmst.music;
 
-public class Song {
+import edu.brown.cs.jmst.beans.Entity;
+
+public class Song extends Entity {
   //TODO: add fields for every field accessible from Spotify API
 }
 
