254e7077b99423db2e9ba80dd87cce374c106cee
samstronghammer <samuel_oliphant@brown.edu>
2018-04-20 13:03:54 -0400

    Adding to constructor for Track

--- src/main/java/edu/brown/cs/jmst/music/Track.java
+++ src/main/java/edu/brown/cs/jmst/music/Track.java
@@ -5,7 +5,7 @@ import java.util.List;
 import edu.brown.cs.jmst.beans.Entity;
 
 public abstract class Track extends Entity {
-  
+
   private Album album;
   private List<String> artistIds;
   private Boolean explicit;
@@ -15,10 +15,16 @@ public abstract class Track extends Entity {
   private int duration_ms;
 
   private String name;
-  
-  public Track(String id, Boolean explicit, int popularity, int duration_ms, List<String> artistIds, Boolean playable ) {
+
+  public Track(String id, Boolean explicit, int popularity, int duration_ms,
+      List<String> artistIds, Boolean playable) {
     this.id = id;
+    this.explicit = explicit;
+    this.popularity = popularity;
+    this.duration_ms = duration_ms;
+    this.artistIds = artistIds;
+    this.playable = playable;
   }
-  
+
   private String url;
 }
