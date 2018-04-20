0c1e341ed74f510c458231d97e72b640b3218441
slimshelly <shell_yang@brown.edu>
2018-04-19 20:39:02 -0400

    make variables private

--- src/main/java/edu/brown/cs/jmst/music/Image.java
+++
@@ -1,7 +0,0 @@
-package edu.brown.cs.jmst.music;
-
-public class Image {
-  int height;
-  String url;
-  int width;
-}
--- src/main/java/edu/brown/cs/jmst/party/PartyGoer.java
+++ src/main/java/edu/brown/cs/jmst/party/PartyGoer.java
@@ -1,5 +1,12 @@
 package edu.brown.cs.jmst.party;
 
-public class PartyGoer {
+import edu.brown.cs.jmst.beans.Entity;
 
+public class PartyGoer extends Entity {
+  
+  // has ID.
+  // has access to song queue
+  
 }
+
+// web playback API.
