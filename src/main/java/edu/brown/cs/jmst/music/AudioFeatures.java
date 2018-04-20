619fb92c5c0f8e858fff5dc5f66de8006cbdfd3a
tvanderm <thomas_vandermillen@brown.edu>
2018-04-19 20:08:41 -0400

    getters for fields in AudioFeatures

--- src/main/java/edu/brown/cs/jmst/music/AudioFeatures.java
+++ src/main/java/edu/brown/cs/jmst/music/AudioFeatures.java
@@ -3,47 +3,137 @@ package edu.brown.cs.jmst.music;
 public class AudioFeatures {
 
   //0.0 - 1.0 confidence track is acoustic. 1.0 is high confidence
-  Float acousticness;
+  private Float acousticness;
 
   //0.0 is least danceable, 1.0 most danceable
-  Float danceability;
+  private Float danceability;
 
   //duration in milliseconds
-  Integer duration_ms;
+  private Integer duration_ms;
 
   //0.0 to 1.0
-  Float energy;
+  private Float energy;
 
   //the closer to 1.0, the greater likelihood the track contains NO vocal content
   //Above 0.5 is meant to represent instrumental tracks, confidence increases towards 1.0
-  Float instrumentalness;
+  private Float instrumentalness;
 
   //0 = C, 1 = C#/Dflat, 2 = D, and so on (Pitch Class notation)
-  Integer key;
+  private Integer key;
 
   //Detects presence of audience in the recording. Value above 0.8 provides strong likelihood that the track is live
-  Float liveness;
+  private Float liveness;
 
   //overall loudness of the track in decibels (dB), typically ranging between -60 and 0 dB
-  Float loudness;
+  private Float loudness;
 
   //major = 1, minor = 0
-  Integer mode;
+  private Integer mode;
 
   //detects presence of spoken words.
   //Above 0.66 = probably made entirely of spoken words
   //between 0.33 and 0.66 = may contain both music and speech, either in sections or layered
-  Float speechiness;
+  private Float speechiness;
 
   //estimated tempo in beats per minute (BPM)
-  Float tempo;
+  private Float tempo;
 
   //estimated overall time signature for the track (beats per bar/measure)
-  Integer time_signature;
+  private Integer time_signature;
 
   //0.0 to 1.0; describes "musical positiveness" conveyed by track.
   //High valence tracks sound more happy
-  Float valence;
+  private Float valence;
 
 
+  /**
+   * @return acousticness
+   */
+  public Float getAcousticness() {
+    return acousticness;
+  }
+
+  /**
+   * @return danceability
+   */
+  public Float getDanceability() {
+    return danceability;
+  }
+
+  /**
+   * @return duration_ms
+   */
+  public Integer getDuration_ms() {
+    return duration_ms;
+  }
+
+  /**
+   * @return energy
+   */
+  public Float getEnergy() {
+    return energy;
+  }
+
+  /**
+   * @return instrumentalness
+   */
+  public Float getInstrumentalness() {
+    return instrumentalness;
+  }
+
+  /**
+   * @return key
+   */
+  public Integer getKey() {
+    return key;
+  }
+
+  /**
+   * @return liveness
+   */
+  public Float getLiveness() {
+    return liveness;
+  }
+
+  /**
+   * @return loudness
+   */
+  public Float getLoudness() {
+    return loudness;
+  }
+
+  /**
+   * @return mode
+   */
+  public Integer getMode() {
+    return mode;
+  }
+
+  /**
+   * @return speechiness
+   */
+  public Float getSpeechiness() {
+    return speechiness;
+  }
+
+  /**
+   * @return tempo
+   */
+  public Float getTempo() {
+    return tempo;
+  }
+
+  /**
+   * @return time_signature
+   */
+  public Integer getTime_signature() {
+    return time_signature;
+  }
+
+  /**
+   * @return valence
+   */
+  public Float getValence() {
+    return valence;
+  }
 }
