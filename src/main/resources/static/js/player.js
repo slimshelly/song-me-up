ffbe7f8780e6c161a7273bc5782335f07d9d2f1c
Maddie Becker <madeleine_becker@brown.edu>
2018-05-03 13:52:00 -0400

    Link player.js to join.ftl

--- src/main/resources/static/js/player.js
+++ src/main/resources/static/js/player.js
@@ -2,130 +2,137 @@ let player;
 
 $(document).ready(() => {
 
-    window.onSpotifyWebPlaybackSDKReady = () => {
-        const token = 'BQDz1KLGOs2A9DHz2JaWJxlwO2-PCmzEKpEcvqWBGud-iz2QLc7GXN2C200DhnuQuGsoxbjNErmT03Sw6iqLp6M2yiLZ79ohPunS4xvPrx_e0B01MvvJ8K_95JgoEVKkg8-ARjBrqvnzmnjLZQc0yh_zEyfW6CZ2YNyBk123baoZPKpGfVglXSzP7w';
-        player = new Spotify.Player({
+        
+    window.onSpotifyWebPlaybackSDKReady = () => {        
+        const token = 'BQApOrqNHoYpLhTN51cw8Pfon_0mcc7qlfdI5HAm0BGKguH4ei1QqPLCWMrYDJFmthWyTwkRW7YT_Y1kPolyz8KJO8hmI1PzXoAIdcmsyBIk9ooYoHrPeND3wPENRJPBOzrQA9SyRghxOh4HywVkSFeDx6HWsZrjIRzP6jWbFgsCexM6g3w0N-KG_g';        
+        player = new Spotify.Player({            
             name: 'Test Player',
-            getOAuthToken: cb => {
-                cb(token);
-            }
+                        getOAuthToken: cb => {                
+                cb(token);            
+            }        
         });
 
-        // Connect to the player!
+                 // Connect to the player!
+                
         player.connect();
 
 
-        player.connect().then(success => {
+                
+        player.connect().then(success => {            
             if (success) {
 
-                player.addListener('ready', ({
-                    device_id
-                }) => {
-                    console.log('Ready with Device ID', device_id);
+                                
+                player.addListener('ready', ({                    
+                    device_id                
+                }) => {                    
+                    console.log('Ready with Device ID', device_id);                
                 });
 
 
-            }
+                            
+            }        
         });
 
 
-        // Error handling
-        player.addListener('initialization_error', ({
-            message
-        }) => {
-            console.error(message);
-        });
-        player.addListener('authentication_error', ({
-            message
-        }) => {
-            console.error(message);
-        });
-        player.addListener('account_error', ({
-            message
-        }) => {
-            console.error(message);
-        });
-        player.addListener('playback_error', ({
-            message
-        }) => {
-            console.error(message);
-        });
-
-        // Playback status updates
-        player.addListener('player_state_changed', state => {
-            console.log(state);
+                 // Error handling
+                
+        player.addListener('initialization_error', ({            
+            message        
+        }) => {            
+            console.error(message);        
+        });        
+        player.addListener('authentication_error', ({            
+            message        
+        }) => {            
+            console.error(message);        
+        });        
+        player.addListener('account_error', ({            
+            message        
+        }) => {            
+            console.error(message);        
+        });        
+        player.addListener('playback_error', ({            
+            message        
+        }) => {            
+            console.error(message);        
         });
 
+                 // Playback status updates
+                
+        player.addListener('player_state_changed', state => {            
+            console.log(state);        
+        });
 
+
+            
     };
 
 });
 
-
-
-const play = ({
+const play = ({    
     spotify_uri,
-    playerInstance: {
-        _options: {
+        playerInstance: {        
+        _options: {            
             getOAuthToken,
-            id
-        }
+                        id        
+        }    
     }
-}) => {
-    getOAuthToken(access_token => {
-        fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
+}) => {    
+    getOAuthToken(access_token => {        
+        fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {            
             method: 'PUT',
-            body: JSON.stringify({
-                uris: [spotify_uri]
+                        body: JSON.stringify({                
+                uris: [spotify_uri]            
             }),
-            headers: {
+                        headers: {                
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${access_token}`
+                                'Authorization': `Bearer ${access_token}`            
             },
-        });
+                    
+        });    
     });
 };
 
 
-function playSong() {
-    play({
+function playSong() {    
+    play({        
         spotify_uri: 'spotify:track:2Th9BGKvfZG8bKQSACitwG',
-        playerInstance: player
+                playerInstance: player    
     });
 
-    console.log("done play")
-    // if success, then play
+        
+    console.log("done play")     // if success, then play
 }
 
 
-function pauseSong() {
-    player.pause().then(() => {
-        console.log('Paused!');
+function pauseSong() {    
+    player.pause().then(() => {        
+        console.log('Paused!');    
     });
 
 }
 
 
-function resumeSong() {
-    player.resume().then(() => {
-        console.log('Resumed!');
+function resumeSong() {    
+    player.resume().then(() => {        
+        console.log('Resumed!');    
     });
 }
 
-function togglePlaySong() {
-    player.togglePlay().then(() => {
-        console.log('Toggled playback!');
+function togglePlaySong() {    
+    player.togglePlay().then(() => {        
+        console.log('Toggled playback!');    
     });
 }
 
-function switchToPrevious() {
-    player.previousTrack().then(() => {
-        console.log('Set to previous track!');
+function switchToPrevious() {    
+    player.previousTrack().then(() => {        
+        console.log('Set to previous track!');    
     });
 }
 
-function switchToNext() {
-    player.nextTrack().then(() => {
-        console.log('Skipped to next track!');
+function switchToNext() {    
+    player.nextTrack().then(() => {        
+        console.log('Skipped to next track!');    
     });
 }
