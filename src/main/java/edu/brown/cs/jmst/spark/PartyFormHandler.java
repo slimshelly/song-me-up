3ad8dc340050ad5d1ab545e842e032349b863707
samstronghammer <samuel_oliphant@brown.edu>
2018-05-07 23:22:47 -0400

    Fixed party form

--- src/main/java/edu/brown/cs/jmst/spark/PartyFormHandler.java
+++ src/main/java/edu/brown/cs/jmst/spark/PartyFormHandler.java
@@ -1,12 +1,19 @@
 package edu.brown.cs.jmst.spark;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.message.BasicNameValuePair;
 
 import com.google.common.collect.ImmutableMap;
 
-import edu.brown.cs.jmst.party.Party;
+import edu.brown.cs.jmst.party.PartyException;
 import edu.brown.cs.jmst.party.User;
 import edu.brown.cs.jmst.songmeup.SmuState;
+import edu.brown.cs.jmst.spotify.SpotifyAuthentication;
+import edu.brown.cs.jmst.spotify.SpotifyException;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -14,15 +21,30 @@ import spark.TemplateViewRoute;
 
 public class PartyFormHandler implements TemplateViewRoute {
 
-	@Override
-	public ModelAndView handle(Request request, Response response) throws Exception {
-	    SmuState state = SmuState.getInstance();
-	    String userid = request.session().attribute("user");
-	    User u = state.getUser(userid);
-	    Party newParty = state.startParty(u);
-	    Map<String,
-	    Object> variables = new ImmutableMap.Builder<String, Object>().put("user_id", u.getId()).build();
-	    return new ModelAndView(variables, "songmeup/host/party_form.ftl");
-	}
+  @Override
+  public ModelAndView handle(Request req, Response res) throws Exception {
+    SmuState state = SmuState.getInstance();
+    String userid = req.session().attribute("user");
+    User u = state.getUser(userid);
+    try {
+      state.startParty(u);
+      Map<String, Object> variables = new ImmutableMap.Builder<String, Object>()
+          .put("user_id", u.getId()).build();
+      return new ModelAndView(variables, "songmeup/host/party_form.ftl");
+    } catch (PartyException e) {
+      SparkErrorEnum err = SparkErrorEnum.ALREADY_IN_PARTY;
+      List<BasicNameValuePair> pair = new ArrayList<>();
+      pair.add(new BasicNameValuePair("error", err.toString()));
+      res.redirect(SpotifyAuthentication.getRootUri() + "/error?"
+          + URLEncodedUtils.format(pair, "UTF-8"));
+    } catch (SpotifyException e) {
+      SparkErrorEnum err = SparkErrorEnum.NEEDS_PREMIUM;
+      List<BasicNameValuePair> pair = new ArrayList<>();
+      pair.add(new BasicNameValuePair("error", err.toString()));
+      res.redirect(SpotifyAuthentication.getRootUri() + "/error?"
+          + URLEncodedUtils.format(pair, "UTF-8"));
+    }
+    return null;
+  }
 
 }
